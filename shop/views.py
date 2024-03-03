0d2c7f69e2c975c7d8f95cf5d1493dae21239e15
aryan patel <210130107060@gecg28.ac.in>
2024-03-03 23:02:59 +0530

    make html page for route

--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,7 @@ import shop.models as model
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from datetime import date
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -154,7 +154,9 @@ def Additem(request):
         mutable_querydict['shop_id'] =str(shop_id)
         uploaded_file = request.FILES['item_image']
         date = datetime.date.today()
-        new_filename = str(shop_id)+'_item_image_'+str(date)+'.jpg'
+        item_name= mutable_querydict['item_name'].lower()
+        print(item_name)
+        new_filename = str(shop_id)+'_'+item_name+'_item_image_'+str(date)+'.jpg'
         form.data = mutable_querydict.copy()
         print(form.data)
         if form.is_valid():
@@ -162,12 +164,35 @@ def Additem(request):
             shop_instance.item_image.save(new_filename, uploaded_file)
             shop_instance.save()
             form.save()
-    
     form = AddItem()
     items = model.Item.objects.filter(shop_id = shop_id)
     data={"items":items,"form":form}
 
     return render(request , 'additem.html', context = data)
 
+
+@login_required(login_url="loginshop")
+def deleteitem(request):
+    if request.method == 'GET':
+        item_id = request.GET.get('itemId')
+        item = model.Item.objects.get(id = item_id)
+        try:
+            # item = model.Item.objects.filter(id = item_id)
+            item_image_path = item.item_image.url
+            if item_image_path != "" :
+                os.unlink('.'+item_image_path)
+            item.delete()
+           
+            shop = request.session['shopdata']['shop_id']
+            shop_id = model.Shop.objects.get(user = shop).id
+            form = AddItem()
+            items = model.Item.objects.filter(shop_id = shop_id)
+            data={"items":items,"form":form}
+            return render(request , 'additem.html', context = data)
+        except item.DoesNotExist:
+            return JsonResponse({'error': 'Item does not exist'}, status=404)
+    
+
+ 
 
     
--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 from django.shortcuts import render,HttpResponse,redirect
 from shop.models import Shop,Item,User
-from geopy.distance import geodesic
+from geopy import distance as dst
+
 # Create your views here.
 
 def userhome(request):
@@ -26,6 +27,7 @@ def userhome(request):
             shop_info = Shop.objects.get(user = shop_user_id)
 
             shop_location =  {
+                'shop_id':shop_user_id,
                 'shop_name':shop_name,
                 'latitude' : shop_info.latitude, 
                 'longitude' : shop_info.longitude}
@@ -38,31 +40,34 @@ def userhome(request):
 
         for station in location_data:
             station_location = station['latitude'], station['longitude'] 
-            distance  = geodesic(user_location,station_location).km
+            print (user_location , station_location)
+            distance  = dst.distance(user_location,station_location).km
+            print(station['shop_id'])
             distance = round(distance, 3)
             station_data  = { 'shop_name' : station['shop_name'],
+                             'shop_id':station['shop_id'],
                              'item_img_url' : station['item_img_url'],
                              'latitude' : station_location[0],
                              'longitude' : station_location[1]}
             
             station_distance[distance] = station_data
+            # print(station_distance)
 
 
         # print(station_distance)
         sorted_location = dict(sorted(station_distance.items(), key=lambda item: item[0]))
-        print(sorted_location)
+        # print(sorted_location)
         return render(request  , 'userhome.html' , context= {'sorted_location'  : sorted_location , 'location_data' :location_data_without_img })
-        # min_distance = min(station_distance)
-        # sorted_stations = (station_distance)
-        # station_cord = station_distance[min_distance]
-        # min_distance = round(min_distance, 3)
-
-        # return HttpResponse(location_data)
-        # return JsonResponse({
-        #     'cord':station_cord,
-        #     'distance':shop,
-        # })
-        # print(shop_info.latitude)
-        #     print()
-        # return HttpResponse()
+       
     return render(request, 'userhome.html')
+
+def getroute(request):
+
+    shop_id = request.GET.get('shop_id')
+    shop_info = Shop.objects.get(user = shop_id)
+    shop_location =  {
+                    'shop_id':shop_id,
+                    'latitude' : shop_info.latitude, 
+                    'longitude' : shop_info.longitude}
+    return render(request, 'showroute.html' , context = {'shop_location':shop_location})
+    
